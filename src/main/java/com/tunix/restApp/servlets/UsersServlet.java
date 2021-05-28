e1524027f568174f39b139866db6972a5abe32fd
worker <pestekhin70@yandex.ru>
2021-05-28 15:31:45 +0700

    userServlet v1

--- src/main/java/com/tunix/restApp/servlets/UsersServlet.java
+++ src/main/java/com/tunix/restApp/servlets/UsersServlet.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-@WebServlet(urlPatterns = "/users")
+@WebServlet(name = "User", urlPatterns = "/users")
 public class UsersServlet extends HttpServlet {
     private UserImplRepository userRepository = new UserImplRepository();
 
@@ -20,30 +20,31 @@ public class UsersServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter printWriter = resp.getWriter();
         List<User> userList = userRepository.getAll();
-        String requestUll = req.getRequestURI();
 
         resp.setContentType("text/html");
         String title = "Database users";
         String docType = "<!DOCTYPE html>";
         printWriter.println(docType + "<html><head><title>" + title + "</title></head><body>");
         printWriter.println("<h1>USERS DATA</h1>");
-        for (User user : userList){
+
+        if(req.getParameter("numberOfUser") == null) {
+            for (User user : userList) {
+                printWriter.println(user);
+                printWriter.println("<br/>");
+            }
+        }else{
+            Long id = Long.parseLong(req.getParameter("param"));
+            User user = userRepository.getById(id);
             printWriter.println(user);
-            printWriter.println("<br/>");
         }
 
-//        if(req.getParameter("numberOfUser") == null) {
-//            printWriter.println(userRepository.getAll());
-//        }else{
-//            printWriter(userRepository.getById(req.getParameter()));
-//        }
-
-        printWriter.println("</html></body>");
+        printWriter.println("</body></html>");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String user = req.getParameter("param");
+//        userRepository.save(user);
     }
 
     @Override
@@ -53,6 +54,7 @@ public class UsersServlet extends HttpServlet {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        Long id = Long.parseLong(req.getParameter("param"));
+        userRepository.deleteById(id);
     }
 }
